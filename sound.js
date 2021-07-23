2957c51c20f598d64771843acf3d7d420da61adb
snipat <32958589+snipat@users.noreply.github.com>
2021-07-23 18:11:34 +0200

    Assets and sound.html

--- sound.js
+++ sound.js
@@ -1,13 +1,7 @@
-function launchAudio() {
 
-  const audio = document.getElementById("au");
+var mySound = document.getElementById("mySound");
 
-  const startPlaying = ()=>{
-    audio.removeEventListener('playing', startPlaying);
-    audio.src = 'https://github.com/mdn/webaudio-examples/blob/master/audio-basics/outfoxing.mp3';
-    audio.play();
-  }
-  audio.addEventListener('playing', startPlaying);
-
+function startGame() {
+  mySound.play();
 }
-launchAudio();
+startGame();
