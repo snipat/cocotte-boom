c172e4452269932938d69ac45f0a3e5b6782c0fc
snipat <32958589+snipat@users.noreply.github.com>
2023-08-24 16:12:04 +0200

    Opacity

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ function changeColor() {
     stateLevel = "bas";
   } else if (pression >= 0 && pression < 500) {
     document.getElementById("jauge").style.color = "green";
-    document.getElementById("round2").style.visibility = "visible";
+    document.getElementById("round2").style.opacity = 1;
 
     stateLevel = "moyen";
   } else if (pression >= 500 && pression < 1000) {
