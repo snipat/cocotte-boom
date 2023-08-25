45f54a71bddc1d9f22cfebb34cacbfe07d7d1f7a
snipat <32958589+snipat@users.noreply.github.com>
2023-08-25 12:41:52 +0200

    Ràz

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function changeColor() {
     stateLevel = "bas";
   } else if (pression >= 0 && pression < 500) {
     document.getElementById("jauge").style.color = "green";
-//    document.getElementById("jaune").style.opacity = "1";
+    document.getElementById("jaune").style.opacity = "1";
     stateLevel = "moyen";
   } else if (pression >= 500 && pression < 1000) {
     document.getElementById("jauge").style.color = "orange";
