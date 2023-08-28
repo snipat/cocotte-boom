9194535c9e3e532a717368ea0d78988e9c530309
snipat <32958589+snipat@users.noreply.github.com>
2023-08-28 20:55:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let beta,
     pression = 0,
     gameover = false,
     stateLevel = "0",
-    angle=0;
+    //angle=0;
 
     const whistle = document.getElementById("whistle");
     const explosion = document.getElementById("explosion");
@@ -76,8 +76,7 @@ function increasePression() {
   } else {
     if ((beta >= 5 && beta < 10) || (beta <= -5 && beta > -10)) {
       pression += 2;
-  //    angle = "low"
-      } else if ((beta >= 10 && beta < 15) || (beta <= -10 && beta > -15)) {
+  //    angle = 1;      } else if ((beta >= 10 && beta < 15) || (beta <= -10 && beta > -15)) {
         pression += 4;
       } else if (beta >= 15 || beta <= -15) {
         pression += 6;
@@ -86,7 +85,7 @@ function increasePression() {
     }
     if ((gamma >= 10 && gamma < 15) || (gamma <= -10 && gamma > -15)) {
       pression += 2;
-    //  angle = "low"
+    //  angle = 2;
       } else if ((gamma >= 15 && gamma < 30) || (gamma <= -15 && gamma > -30)) {
         pression += 4;
       } else if (gamma >= 30 || gamma <= -30) {
