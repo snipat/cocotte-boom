ff747dc4acbccbe765a2c5ff0b92c46c43b25a0f
snipat <32958589+snipat@users.noreply.github.com>
2023-08-29 12:23:53 +0200

    Fix

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function changeColor() {
   } else if (pression >= 500 && pression < 1000) {
     document.getElementById("jauge").style.color = "orange";
     document.getElementById("orange").style.opacity = "1";
-    element.classList.replace(base,bouge);
+    img.classList.replace(base,bouge);
     //audio2.play();
   } else if (pression >= 1000 && pression <= 2000) {
     document.getElementById("red").style.opacity = "1";
