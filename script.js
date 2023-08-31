d26a56af44b7e78d593513e4bdff1317639069ec
snipat <32958589+snipat@users.noreply.github.com>
2023-08-31 21:27:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -276,7 +276,7 @@ function gameplay() {
     document.getElementById("jauge").style.color = "orange";
     document.getElementById("orange").style.opacity = "1";
     cocotte.classList.replace('base','bouge');
-    window.pace('sound' + 2, false);
+    window.pace('sound' + n, false);
   } else if (pression >= 1000 && pression <= 2000) {
     document.getElementById("red").style.opacity = "1";
   } else {
