64f4e55d847f2fa405a0af5dbde9784313c2cd4f
snipat <32958589+snipat@users.noreply.github.com>
2023-09-01 14:03:50 +0200

    OK

--- script.js
+++ script.js
@@ -276,7 +276,7 @@ function changeColor() {
     document.getElementById("jauge").style.color = "orange";
     document.getElementById("orange").style.opacity = "1";
     cocotte.classList.replace('base','bouge');
-    // pace();
+    getElementById("game")pace();
   } else if (pression >= 1000 && pression <= 2000) {
     document.getElementById("red").style.opacity = "1";
     // pace('sound' + 2, false);
