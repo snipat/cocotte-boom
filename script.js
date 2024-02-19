8c9f94469de322b806def2dfd02458e955031cd0
snipat <32958589+snipat@users.noreply.github.com>
2024-02-19 10:23:21 +0100

    Script copy increase Pression

--- script.js
+++ script.js
@@ -67,3 +67,80 @@ let loop = new SeamlessLoop();
         console.error(e);
       });
   }
+
+
+
+  function increasePression() {
+    if (gameover) {
+    } else {
+      if ((beta >= 5 && beta < 10) || (beta <= -5 && beta > -10)) {
+        pression += 2;
+        } else if ((beta >= 10 && beta < 15) || (beta <= -10 && beta > -15)) {
+          pression += 4;
+        } else if (beta >= 15 || beta <= -15) {
+          pression += 6;
+      } else {
+        pression += 1;
+      }
+      if ((gamma >= 10 && gamma < 15) || (gamma <= -10 && gamma > -15)) {
+        pression += 2;
+        } else if ((gamma >= 15 && gamma < 30) || (gamma <= -15 && gamma > -30)) {
+          pression += 4;
+        } else if (gamma >= 30 || gamma <= -30) {
+          pression += 6;
+      } else {
+        pression += 1;
+      }
+    }
+  }
+
+  // function increment(){
+  //     pression=pression+100;
+  //     console.log("increment"+ pression)
+  // }
+
+  function changeColor() {
+
+
+    if (pression == 0) {
+      document.getElementById("jauge").style.color = "purple";
+    } else if (pression >= 0 && pression < 500) {
+      document.getElementById("jauge").style.color = "green";
+    } else if (pression >= 500 && pression < 1000) {
+      document.getElementById("ambiance").pause();
+  // enlever JQUERY
+      // audio.pause();
+      // audio.currentTime = 0;
+      // document.getElementById("ambiancemid").play();
+      document.getElementById("jauge").style.color = "orange";
+      document.getElementById("orange").style.opacity = "1";
+      cocotte.classList.replace('base','bouge');
+      // pace();
+    } else if (pression >= 1000 && pression <= 2000) {
+      document.getElementById("red").style.opacity = "1";
+    } else {
+      gameover = true;
+      document.getElementById("explosion").play();
+    }
+  }
+
+  function changeAngle(){
+    if((beta >= 5 && beta < 10) || (beta <= -5 && beta > -10)){
+      document.getElementById("gameZone").style.backgroundColor="green";
+    }
+    else if ((beta >= 10 && beta < 15) || (beta <= -10 && beta > -15)) {
+      document.getElementById("gameZone").style.backgroundColor="orange";
+    }
+    else if (beta >= 15 || beta <= -15) {
+      document.getElementById("gameZone").style.backgroundColor="red";
+    }
+    if ((gamma >= 10 && gamma < 15) || (gamma <= -10 && gamma > -15)) {
+      document.getElementById("gameZone").style.backgroundColor="green";
+    }
+    else if ((gamma >= 15 && gamma < 30) || (gamma <= -15 && gamma > -30)) {
+      document.getElementById("gameZone").style.backgroundColor="orange";
+    }
+    else if (gamma >= 30 || gamma <= -30) {
+      document.getElementById("gameZone").style.backgroundColor="red";
+    }
+  }
