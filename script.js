8d837da8d55c717668f271b8b8ba285f35e04275
snipat <32958589+snipat@users.noreply.github.com>
2024-03-15 15:20:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -275,17 +275,17 @@ function changeColor() {
     document.getElementById("jauge").style.color = "purple";
   } else if (pression >= 0 && pression < 500) {
     document.getElementById("jauge").style.color = "green";
-  } else if (pression >= 500 && pression < 1500) {
+  } else if (pression >= 500 && pression < 1000) {
     document.getElementById("jauge").style.color = "orange";
     document.getElementById("orange").style.opacity = "1";
     document.getElementById("ambiance").pause();
     document.getElementById("ambiancemid").play();
     cocotte.classList.replace('base','bouge');
-  } else if (pression >= 1500 && pression <= 2500) {
+  } else if (pression >= 1000 && pression <= 2000) {
     document.getElementById("red").style.opacity = "1";
     document.getElementById("ambiancemid").pause();
     document.getElementById("ambiancehard").play();
-  } else if (pression > 3000) {
+  } else if (pression > 2500) {
     document.getElementById("boom").play();
     document.getElementById("ambiancehard").pause();
     document.getElementById("explosion").style.display = "block";
