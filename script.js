689edfea0bdab5a0e28f71df4a48eca4922a22ae
snipat <32958589+snipat@users.noreply.github.com>
2026-06-30 16:53:13 +0200

    centrage gif

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("splash-version").innerHTML = v;
 });
 
-const VERSION = 20;
+const VERSION = 21;
 
 let beta,
     gamma,
