daf31de5b4c7df62c5b03dbb7e2c73ed7252a8de
snipat <32958589+snipat@users.noreply.github.com>
2026-06-30 17:37:45 +0200

    v27 New snds

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("splash-version").innerHTML = v;
 });
 
-const VERSION = 26;
+const VERSION = 27;
 
 let beta,
     gamma,
