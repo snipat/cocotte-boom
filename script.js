138cd6a6f5115e01d48ef237d5847897332c7667
snipat <32958589+snipat@users.noreply.github.com>
2026-06-30 17:20:40 +0200

    v25 Correction rotate

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("splash-version").innerHTML = v;
 });
 
-const VERSION = 24;
+const VERSION = 25;
 
 let beta,
     gamma,
