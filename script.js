44e2ca05237086dfaaf4755baddb29c0b24f9435
snipat <32958589+snipat@users.noreply.github.com>
2026-07-01 15:48:17 +0200

    v53 Wrapper div pour centrage cocotte, animations rotate() seul

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("splash-version").innerHTML = v;
 });
 
-const VERSION = 52;
+const VERSION = 53;
 
 let beta,
     gamma,
