efcfd2cce5334c360b5279eb8f8b4030a9386094
snipat <32958589+snipat@users.noreply.github.com>
2026-07-01 13:57:37 +0200

    v37 Update launch.json

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("splash-version").innerHTML = v;
 });
 
-const VERSION = 36;
+const VERSION = 37;
 
 let beta,
     gamma,
