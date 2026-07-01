ea2c3334a9d4602902ddaedb5ce2fa5bf02cd8cf
snipat <32958589+snipat@users.noreply.github.com>
2026-07-01 15:13:52 +0200

    v50 Push

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("splash-version").innerHTML = v;
 });
 
-const VERSION = 49;
+const VERSION = 50;
 
 let beta,
     gamma,
